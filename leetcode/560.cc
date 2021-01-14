5d428647046662ea8ad50e06bcc7e913adf591b5
cke0devel <cke0devel@gmail.com>
2021-01-14 15:52:51 +0900

    [leetcode/wa] 560. Subarray Sum Equals K

--- leetcode/560.cc
+++ leetcode/560.cc
@@ -1,21 +1,20 @@
 class Solution {
-public:
-	int subarraySum(vector<int>& nums, int k) {
-		vector<int> accu = {0};
+	public:
+		int subarraySum(vector<int>& nums, int k) {
+			unordered_set<int> sums = {0};
 
-		for(const auto n : nums) {
-			accu.push_back(accu.back() + n);
-		}
+			int sum = 0;
+			int cnt = 0;
+			for(const auto n : nums) {
+				sum += n;
 
-		int cnt = 0;
-		for(int i=0; i<accu.size(); i++) {
-			for(int j=i+1; j<accu.size(); j++) {
-				if(accu[j] - accu[i] == k) {
+				if(sums.find(sum-k) != sums.end()) {
 					cnt += 1;
 				}
-			}
-		}
 
-		return cnt;
-	}
+				sums.insert(sum);
+			}
+
+			return cnt;
+		}
 };
