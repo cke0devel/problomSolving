7a2f7aa621dce833f2dcb529c32d2ce00f0dcbe6
cke0devel <cke0devel@gmail.com>
2021-01-16 07:30:38 +0900

    [leetcode/ac] 326. Power of Three
    Runtime: 4 ms, faster than 98.14% of C++ online submissions for Power of Three.
    Memory Usage: 5.7 MB, less than 100.00% of C++ online submissions for Power of Three.

--- leetcode/solved/326.cc
+++ leetcode/solved/326.cc
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        const set<int> powerOfThree = {
-            1, 3, 9, 27, 81,
-            243, 729, 2187, 6561, 19683,
-            59049, 177147, 531441, 1594323, 4782969,
-            14348907, 43046721, 129140163, 387420489, 1162261467
-        };
+        if(n<=0) return false;
         
-        return powerOfThree.find(n) != powerOfThree.end();
+        while(n>1) {
+            if(n%3 != 0) return false;
+            n /= 3;
+        }
+        
+        return true;
     }
 };
