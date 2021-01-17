e91ffc236a34d416239b3efc7081773cb466a6fb
cke0devel <cke0devel@gmail.com>
2021-01-17 22:11:53 +0900

    [leetcode/ac] 88. Merge Sorted Array
    Runtime: 4 ms, faster than 69.74% of C++ online submissions for Merge Sorted Array.
    Memory Usage: 9.1 MB, less than 95.31% of C++ online submissions for Merge Sorted Array.

--- leetcode/solved/88.cc
+++ leetcode/solved/88.cc
@@ -1,30 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(n == 0) {
-            return;
-        }
-        if(m==0) {
-            copy(begin(nums2), end(nums2), begin(nums1));
-            return;
-        }
-        
-        int cur = m+n - 1;
-        m -= 1;
-        n -= 1;
-        while(m>=0 && n>=0) {
-            if(nums1[m] >= nums2[n]) {
-                nums1[cur] = nums1[m];
-                m--;
-            } else {
-                nums1[cur] = nums2[n];
-                n--;
-            }
-            cur -= 1;
-        }
-        
-        if(n>=0) {
-            copy(begin(nums2), begin(nums2)+n+1, begin(nums1));
-        }
+        copy(begin(nums2), end(nums2), begin(nums1)+m);
+        sort(begin(nums1), end(nums1));
     }
 };
