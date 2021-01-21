bb32ddca8e90ef7c53cb7d05401a4ca91a5d249f
cke0devel <cke0devel@gmail.com>
2021-01-21 11:39:30 +0900

    [leetcode/wa] 75. Sort Colors

---
+++ leetcode/75.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int red = 0;
+        int blue = nums.size() - 1;
+        
+        int cur = 0;
+        while(cur < nums.size()) {
+            if(nums[cur]==0 && cur>red) {
+                swap(nums[cur], nums[red]);
+                red += 1;
+                cur += 1;
+            } else if(nums[cur]==2 && cur<blue) {
+                swap(nums[cur], nums[blue]);
+                blue -= 1;
+            } else {
+                cur += 1;
+            }
+        }
+    }
+};
