c9d15d5ed3178a45a8833f27e60c160ed1a8bf0f
cke0devel <cke0devel@gmail.com>
2021-01-21 22:51:48 +0900

    [leetcode/tle] 1673. Find the Most Competitive Subsequence

--- leetcode/1673.cc
+++ leetcode/1673.cc
@@ -14,10 +14,15 @@ public:
         for(auto it=cbegin(pos); k>0; ++it) {
             auto [n, i] = *it;
             
-            if(lower<i && sz-k >= i) {
-                ans.push_back(n);
-                k--;
-                lower = i;
+            if(lower<i) {
+                if(sz-k > i) {
+                    ans.push_back(n);
+                    k--;
+                    lower = i;
+                } else if(sz-k == i) {
+                    copy(begin(nums)+i, end(nums), back_inserter(ans));
+                    k = 0;
+                }
             }
         }
         
