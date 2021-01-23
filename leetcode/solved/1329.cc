8ea434854426967ee464604b16e7f602e99e1506
cke0devel <cke0devel@gmail.com>
2021-01-24 00:05:19 +0900

    [leetcode/ac] 1329. Sort the Matrix Diagonally
    Runtime: 16 ms
    Memory Usage: 8.7 MB
    
    Your runtime beats 85.76 % of cpp submissions.
    Your memory usage beats 99.33 % of cpp submissions.

--- leetcode/solved/1329.cc
+++ leetcode/solved/1329.cc
@@ -10,22 +10,16 @@ private:
         diagonalIterator(vector<vector<int>> *mat, const int starty, const int startx) :
             mat(mat), curx(startx), cury(starty)
         {
-            if(curx >= (*mat)[0].size() || cury >= mat->size()) {
-                curx = (*mat)[0].size();
-                cury = mat->size();
-            }
         }
         
-        reference operator*() const { return (*mat)[cury][curx]; }
+        reference operator*() const {
+            return (*mat)[cury][curx];
+        }
         pointer operator->() { return &(*mat)[cury][curx]; }
         
         diagonalIterator& operator++() {
             curx++;
             cury++;
-            if(curx >= (*mat)[0].size() || cury >= mat->size()) {
-                curx = (*mat)[0].size();
-                cury = mat->size();
-            }
             return *this;
         }  
         diagonalIterator operator++(int) {
@@ -43,7 +37,7 @@ private:
             return this->curx==that.curx && this->cury==that.cury;
         }
         bool operator!=(const diagonalIterator &that) const {
-            return !(*this == that);
+            return this->curx!=that.curx || this->cury!=that.cury;
         }
         difference_type operator-(const diagonalIterator &that) const {
             return this->curx - that.curx;
@@ -68,17 +62,18 @@ private:
         return diagonalIterator(&mat, starty, startx);
     }
     
-    diagonalIterator dend(vector<vector<int>> &mat) {
-        return diagonalIterator(&mat, mat.size(), mat[0].size());
+    diagonalIterator dend(vector<vector<int>> &mat, const int starty, const int startx) {
+        const int delta = min(mat.size()-starty, mat[0].size()-startx);
+        return diagonalIterator(&mat, starty+delta, startx+delta);
     }
     
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
         for(int i=0; i<mat.size(); i++) {
-            sort(dbegin(mat, i, 0), dend(mat));
+            sort(dbegin(mat, i, 0), dend(mat, i, 0));
         }
         for(int i=1; i<mat[0].size(); i++) {
-            sort(dbegin(mat, 0, i), dend(mat));
+            sort(dbegin(mat, 0, i), dend(mat, 0, i));
         }
         
         return mat;
