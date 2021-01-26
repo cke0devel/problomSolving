d6063dfc84798531a9eb1f223aad774764e9d8f0
cke0devel <cke0devel@gmail.com>
2021-01-26 23:02:09 +0900

    [leetcode/wa] 747. Largest Number At Least Twice of Others

---
+++ leetcode/747.cc
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        if(nums.size() <= 1) {
+            return 0;
+        }
+        
+        int first = 0;
+        int second = 1;
+
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] > nums[first]) {
+                second = first;
+                first = i;
+            }
+        }
+        
+        if(nums[second]*2 > nums[first]) {
+            return -1;
+        }
+        
+        return first;
+    }
+};
