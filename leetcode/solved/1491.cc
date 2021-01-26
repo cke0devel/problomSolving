937d51105fda8bc6237bdadb1f04a9f4c697d47b
cke0devel <cke0devel@gmail.com>
2021-01-26 15:09:06 +0900

    [leetcode/ac] 1491. Average Salary Excluding the Minimum and Maximum Salary
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Average Salary Excluding the Minimum and Maximum Salary.
    Memory Usage: 7.2 MB, less than 78.30% of C++ online submissions for Average Salary Excluding the Minimum and Maximum Salary.

--- leetcode/solved/1491.cc
+++ leetcode/solved/1491.cc
@@ -1,8 +1,16 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
-        sort(begin(salary), end(salary));
+        int low = 1000000;
+        int high = 1000;
         
-        return accumulate(begin(salary)+1, end(salary)-1, 0) / (salary.size()-2.);
+        int total = 0;
+        for(const auto n : salary) {
+            total += n;
+            low = min(low, n);
+            high = max(high, n);
+        }
+        
+        return (total-low-high) / (salary.size()-2.);
     }
 };
