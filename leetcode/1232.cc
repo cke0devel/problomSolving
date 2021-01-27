b28b689be2bd735f2896f6bd3c9d6f33d0f783f9
cke0devel <cke0devel@gmail.com>
2021-01-27 13:52:34 +0900

    [leetcode/wa] 1232. Check If It Is a Straight Line

---
+++ leetcode/1232.cc
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int dx = coordinates[0][0] - coordinates[1][0];
+        int dy = coordinates[0][1] - coordinates[1][1];
+        
+        dx /= gcd(dx,dy);
+        dy /= gcd(dx,dy);
+        
+        for(int i=2; i<coordinates.size(); i++) {
+            int x = coordinates[0][0] - coordinates[i][0];
+            int y = coordinates[0][1] - coordinates[i][1];
+            const int k = gcd(x,y);
+            
+            x /= k;
+            y /= k;
+            
+            if(dx!=x || dy!=y) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
