5645723f4fe7edc4b2f59eba34fc8b5a02b3779e
cke0devel <cke0devel@gmail.com>
2021-01-27 22:45:43 +0900

    [leetcode/wa] 222. Count Complete Tree Nodes

---
+++ leetcode/222.cc
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == nullptr) return 0;
+        
+        return countNodes(root, 1);
+    }
+    
+private:
+    int countNodes(TreeNode* root, int idx) {
+        if(root->right) {
+            return countNodes(root->right, idx*2+1);
+        }
+        if(root->left) {
+            return countNodes(root->left, idx*2);
+        }
+        
+        return idx;
+    }
+};
