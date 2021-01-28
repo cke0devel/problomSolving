9d1fe64fd4b0622a497088b4d2bd0e2d6bb2b560
cke0devel <cke0devel@gmail.com>
2021-01-28 22:16:43 +0900

    [leetcode/wa] 1663. Smallest String With A Given Numeric Value

---
+++ leetcode/1663.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        int z = k/26;
+        k %= 26;
+        
+        if(z+k < n) {
+            const int r = (n-(z+k)+25)/26;
+            z -= r;
+            k += r*26;
+        }
+
+        n -= z;
+
+        return string(n-1, 'a') + char('a'+k-(n-1)-1) + string(z, 'z');
+    }
+};
