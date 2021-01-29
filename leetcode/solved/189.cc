bc1f3a0c40d59d0ceddc7d6e9929dbbb01aa65aa
cke0devel <cke0devel@gmail.com>
2021-01-29 22:46:37 +0900

    [leetcode/ac] 189. Rotate Array
    Runtime: 4 ms, faster than 93.17% of C++ online submissions for Rotate Array.
    Memory Usage: 9.8 MB, less than 88.95% of C++ online submissions for Rotate Array.

--- leetcode/solved/189.cc
+++ leetcode/solved/189.cc
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
         std::rotate(begin(nums), begin(nums)+(nums.size()-k), end(nums));
     }
 };
