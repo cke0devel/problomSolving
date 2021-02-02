3e8cd1b8805597477ad23103ba371993565e5b2c
cke0devel <cke0devel@gmail.com>
2021-02-02 22:58:35 +0900

    [leetcode/ac] 264. Ugly Number II
    Runtime: 164 ms, faster than 21.63% of C++ online submissions for Ugly Number II.
    Memory Usage: 29.6 MB, less than 15.79% of C++ online submissions for Ugly Number II.

--- leetcode/solved/264.cc
+++ leetcode/solved/264.cc
@@ -2,16 +2,14 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         set<long> s = {1};
-        priority_queue<long, std::vector<long>, std::greater<long>> q;
-        q.push(1);
         
         long ans = 0;
         while(n--) {
-            ans = q.top(); q.pop();
+            ans = *begin(s); s.erase(begin(s));
 
-            if(s.insert(ans*2).second) q.push(ans*2);
-            if(s.insert(ans*3).second) q.push(ans*3);
-            if(s.insert(ans*5).second) q.push(ans*5);
+            s.insert(ans*2);
+            s.insert(ans*3);
+            s.insert(ans*5);
         }
         
         return ans;
