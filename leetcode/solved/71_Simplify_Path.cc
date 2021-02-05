3a4304cd98680ef413671bd6c607ff4429b1f064
cke0devel <cke0devel@gmail.com>
2021-02-05 22:16:23 +0900

    [leetcode/ac] 71. Simplify Path
    Runtime: 8 ms
    Memory Usage: 11.4 MB
    
    Your runtime beats 60.74 % of cpp submissions.
    Your memory usage beats 12.30 % of cpp submissions.

---
+++ leetcode/solved/71_Simplify_Path.cc
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        deque<string> k;
+        
+        path += '/';
+        string name;
+        for(const auto ch : path) {
+            if(ch == '/') {
+                if(name == "..") {
+                    if(!k.empty()) {
+                        k.pop_back();
+                    }
+                } else if(!name.empty() && name!=".") {
+                    k.push_back(name);
+                }
+                
+                name = "";
+            } else {
+                name += ch;
+            }
+        }
+        
+        if(k.empty()) {
+            return "/";
+        }
+        
+        return accumulate(begin(k), end(k), string(""),
+                          [](const string &a, const string &b) {
+                              return a+'/'+b;
+                          });
+    }
+};
