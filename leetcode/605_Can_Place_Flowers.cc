60c45dd318fc992496c755129a8ca8eb2180ab8a
cke0devel <cke0devel@gmail.com>
2021-02-06 22:33:44 +0900

    [leetcode/wa] 605. Can Place Flowers

--- leetcode/605_Can_Place_Flowers.cc
+++ leetcode/605_Can_Place_Flowers.cc
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        if(flowerbed.size()==1 && flowerbed[0]==0) {
-            flowerbed[0] = 1;
-            n--;
-        }
-        if(flowerbed.size()>=2 && flowerbed[0]==0 && flowerbed[1]==0) {
-            flowerbed[0] = 1;
-            n--;
-        }
-        if(flowerbed.size()>=2 && flowerbed.back()==0 && flowerbed[flowerbed.size()-2]==0) {
-            flowerbed.back() = 1;
-            n--;
-        }
+        int pp = 0;
+        int p = flowerbed[0];
         
-        for(int i=2; i<flowerbed.size() && n>0; i++) {
-            if(flowerbed[i-2]==0 && flowerbed[i-1]==0 && flowerbed[i]==0) {
+        for(int i=1; i<flowerbed.size() && n>0; i++) {
+            if(pp==0 && p==0 && flowerbed[i]==0) {
                 flowerbed[i-1] = 1;
                 n--;
             }
+            
+            pp = p;
+            p = flowerbed[i];
+        }
+        
+        if(n>0 && pp==0 && p==0) {
+            flowerbed.back() = 1;
+            n--;
         }
         
         return n==0;
