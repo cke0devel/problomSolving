4ea5f989b71474d3948a98764f435a1d2055c0ed
cke0devel <cke0devel@gmail.com>
2021-02-12 22:24:21 +0900

    [leetcode/ac] 530. Minimum Absolute Difference in BST
    Runtime: 20 ms, faster than 70.40% of C++ online submissions for Minimum Absolute Difference in BST.
    Memory Usage: 25.6 MB, less than 35.80% of C++ online submissions for Minimum Absolute Difference in BST.

---
+++ leetcode/solved/530_Minimum_Absolute_Difference_in_BST.cc
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getMinimumDifference(TreeNode* root) {
+        vector<int> values;
+        
+        inorderTraversal(root, back_inserter(values));
+        
+        int ans = values[1] - values[0];
+        
+        for(int i=2; i<values.size(); i++) {
+            ans = min(ans, values[i]-values[i-1]);
+        }
+        
+        return ans;
+    }
+    
+private:
+    template<class ForwordIter>
+    void inorderTraversal(TreeNode* root, ForwordIter iter) {
+        if(root == nullptr) {
+            return;
+        }
+        
+        inorderTraversal(root->left, iter);
+        *iter = root->val;
+        inorderTraversal(root->right, iter);
+    }
+};
