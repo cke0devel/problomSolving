335315686fcfe6ebb1128f1c1d621c75ccf42dc7
cke0devel <cke0devel@gmail.com>
2021-02-16 22:23:38 +0900

    [leetcode/ac] 47. Permutations II
    Runtime: 4 ms, faster than 96.37% of C++ online submissions for Permutations II.
    Memory Usage: 8.4 MB, less than 97.48% of C++ online submissions for Permutations II.

---
+++ leetcode/solved/47_Permutations_II.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+
+        vector<vector<int>> ans;
+        
+        do {
+            ans.push_back(nums);
+        } while(next_permutation(begin(nums), end(nums)));
+        
+        return ans;
+    }
+};
