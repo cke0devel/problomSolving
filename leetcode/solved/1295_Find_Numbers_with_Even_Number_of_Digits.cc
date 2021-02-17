48ee6b190258467294d7880991c53fc858ee14ce
cke0devel <cke0devel@gmail.com>
2021-02-18 00:05:49 +0900

    [leetcode/ac] 1295. Find Numbers with Even Number of Digits
    Runtime: 4 ms, faster than 95.88% of C++ online submissions for Find Numbers with Even Number of Digits.
    Memory Usage: 9.7 MB, less than 98.75% of C++ online submissions for Find Numbers with Even Number of Digits.

--- leetcode/solved/1295_Find_Numbers_with_Even_Number_of_Digits.cc
+++ leetcode/solved/1295_Find_Numbers_with_Even_Number_of_Digits.cc
@@ -3,7 +3,7 @@ public:
     int findNumbers(vector<int>& nums) {
         return count_if(begin(nums), end(nums),
                        [](const int n) {
-                           return (10<=n && n<100) || (1000<=n && n<100000);
+                           return (10<=n && n<100) || (1000<=n && n<10000) || (n==100000);
                        });
     }
 };
