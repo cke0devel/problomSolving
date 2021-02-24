1e12e5fce8cc93507e641e13fb7595822a5945f0
cke0devel <cke0devel@gmail.com>
2021-02-24 23:13:56 +0900

    [leetcode/ac] 1464. Maximum Product of Two Elements in an Array
    Runtime: 4 ms, faster than 94.71% of C++ online submissions for Maximum Product of Two Elements in an Array.
    Memory Usage: 10 MB, less than 73.48% of C++ online submissions for Maximum Product of Two Elements in an Array.

--- leetcode/solved/1464_Maximum_Product_of_Two_Elements_in_an_Array.cc
+++ leetcode/solved/1464_Maximum_Product_of_Two_Elements_in_an_Array.cc
@@ -1,17 +1,8 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        transform(begin(nums), end(nums), begin(nums),
-                 [](const int n) { return n-1; });
+        sort(begin(nums), end(nums), greater<int>());
         
-        int ans = 0;
-        
-        for(int i=0; i<nums.size(); i++) {
-            for(int j=i+1; j<nums.size(); j++) {
-                ans = max(ans, nums[i]*nums[j]);
-            }
-        }
-        
-        return ans;
+        return (nums[0]-1) * (nums[1]-1);
     }
 };
