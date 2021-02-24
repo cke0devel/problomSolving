a3fc2ca1c1eb364a393fad023e3938954f835921
cke0devel <cke0devel@gmail.com>
2021-02-24 22:15:26 +0900

    [leetcode/re] 14. Longest Common Prefix

---
+++ leetcode/14_Longest_Common_Prefix.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans;
+        
+        for(int p=0; p<strs[0].size(); p++) {
+            bool matched = true;
+            
+            for(int i=1; i<strs.size(); i++) {
+                if(strs[i].size() <= p || strs[i-1][p] != strs[i][p]) {
+                    matched = false;
+                    break;
+                }
+            }
+            
+            if(matched) {
+                ans += strs[0][p];
+            }
+        }
+        
+        return ans;
+    }
+};
