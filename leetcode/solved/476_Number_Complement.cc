109b9b71969977da31331ada4651011c650ee159
cke0devel <cke0devel@gmail.com>
2021-02-27 22:50:22 +0900

    [leetcode/ac] 476. Number Complement
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number Complement.
    Memory Usage: 5.9 MB, less than 65.56% of C++ online submissions for Number Complement.

--- leetcode/solved/476_Number_Complement.cc
+++ leetcode/solved/476_Number_Complement.cc
@@ -3,7 +3,7 @@ public:
     int findComplement(int num) {
         if(num==0) return 1;
         
-        const int mask = (1 << ((sizeof(num)*8) - __builtin_clz(num))) - 1;
+        const int mask = (1U << ((sizeof(num)*8) - __builtin_clz(num))) - 1;
         
         return (~num) & mask;
     }
