4f70eef1840a64896fe4555fa9fd35c6cd421238
cke0devel <cke0devel@gmail.com>
2021-02-27 22:46:57 +0900

    [leetcode/ac] 1009. Complement of Base 10 Integer
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Complement of Base 10 Integer.
    Memory Usage: 6 MB, less than 65.63% of C++ online submissions for Complement of Base 10 Integer.

--- leetcode/solved/1009_Complement_of_Base_10_Integer.cc
+++ leetcode/solved/1009_Complement_of_Base_10_Integer.cc
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int bitwiseComplement(int N) {
+        if(N==0) return 1;
+        
         const int mask = (1 << ((sizeof(N)*8) - __builtin_clz(N))) - 1;
         
         return (~N) & mask;
