85de98affd4317a45d2a68d49edce72377484718
cke0devel <cke0devel@gmail.com>
2021-02-28 22:08:06 +0900

    [leetcode/ac] 590. N-ary Tree Postorder Traversal
    Runtime: 20 ms, faster than 93.32% of C++ online submissions for N-ary Tree Postorder Traversal.
    Memory Usage: 11.1 MB, less than 94.01% of C++ online submissions for N-ary Tree Postorder Traversal.

---
+++ leetcode/solved/590_N-ary_Tree_Postorder_Traversal.cc
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+
+		postorder(root, &ans);
+
+		return ans;
+    }
+    
+private:
+	void postorder(Node* root, vector<int> *ans) {
+		if(root == nullptr) {
+			return;
+		}
+
+		for(const auto node : root->children) {
+			postorder(node, ans);
+		}
+        
+        ans->push_back(root->val);
+	} 
+};
