48922d8f510c4021edcd0ff36b13945af4b12f83
cke0devel <cke0devel@gmail.com>
2021-03-02 22:40:52 +0900

    [leetcode/ac] 1422. Maximum Score After Splitting a String
    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Maximum Score After Splitting a String.
    Memory Usage: 6.6 MB, less than 41.38% of C++ online submissions for Maximum Score After Splitting a String.

--- leetcode/solved/1422_Maximum_Score_After_Splitting_a_String.cc
+++ leetcode/solved/1422_Maximum_Score_After_Splitting_a_String.cc
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int maxScore(string s) {
-        vector<int> zeros(s.size());
-        vector<int> ones(s.size());
+        vector<int> zeros(s.size()-1);
+        vector<int> ones(s.size()-1);
 
         zeros[0] = (s[0]=='0') ? 1 : 0;
-        ones.back() = s.back()-'0';
+        ones[s.size()-2] = s.back()-'0';
         for(int i=1; i<zeros.size(); i++) {
             zeros[i] = zeros[i-1] + ((s[i]=='0') ? 1 : 0);
-            ones[s.size()-i-1] = ones[s.size()-i] + (s[s.size()-i-1]-'0');
+            ones[s.size()-i-2] = ones[s.size()-i-1] + (s[s.size()-i-1]-'0');
         }
         
         int ans = 0;
-        for(int i=1; i<s.size()-1; i++) {
+        for(int i=0; i<zeros.size(); i++) {
             ans = max(ans, zeros[i]+ones[i]);
         }
         
