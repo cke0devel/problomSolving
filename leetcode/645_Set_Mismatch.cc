cf5ea6aaffd16b8354f9c8987989afbbbd03a5a5
cke0devel <cke0devel@gmail.com>
2021-03-02 22:06:03 +0900

    [leetcode/wa] 645. Set Mismatch

---
+++ leetcode/645_Set_Mismatch.cc
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+
+        int repetition = -1;
+        int loss = -1;
+        
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i-1] == nums[i]) repetition = nums[i];
+            if(nums[i]-nums[i-1] > 1) loss = nums[i]-1;
+            
+            if(repetition>0 && loss>0) {
+                break;
+            }
+        }
+        
+        if(loss == -1) loss = nums.size();
+        
+        return {repetition, loss};
+    }
+};
