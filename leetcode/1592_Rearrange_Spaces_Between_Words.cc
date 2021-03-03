16cc444760a87fbfd304ed0ac89a89341ac3ad6d
cke0devel <cke0devel@gmail.com>
2021-03-03 23:15:11 +0900

    [leetcode/re] 1592. Rearrange Spaces Between Words

---
+++ leetcode/1592_Rearrange_Spaces_Between_Words.cc
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reorderSpaces(string text) {
+        int space = count_if(begin(text), end(text),
+                             [](const char c) { return c==' '; });
+        
+        if(space == 0) {
+            return text;
+        }
+        
+        vector<string> words;
+        istringstream sin(text);
+        copy(istream_iterator<string>(sin), istream_iterator<string>(),
+             back_inserter(words));
+
+        const int blank = space / (words.size()-1);
+        
+        string ans;
+        for(const string &word : words) {
+            ans += word + string(min(blank, space), ' ');
+            space -= blank;
+        }
+        
+        return ans;
+    }
+};
