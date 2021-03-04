b0a4f8eb9d1cb4c2c36a4e659002ab41a555c212
cke0devel <cke0devel@gmail.com>
2021-03-04 23:37:43 +0900

    [leetcode/wa] 1636. Sort Array by Increasing Frequency

---
+++ leetcode/1636_Sort_Array_by_Increasing_Frequency.cc
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        map<int,int> freq;
+        
+        for(const auto n : nums) {
+            freq[n] += 1;
+        }
+        
+        vector<pair<int,int>> v(begin(freq), end(freq));
+        sort(begin(v), end(v),
+            [](const auto &lhs, const auto &rhs) {
+                if(lhs.second == rhs.second) {
+                    return lhs.first > rhs.second;
+                }
+                return lhs.second < rhs.second;
+            });
+        
+        vector<int> ans;
+        for(const auto e : v) {
+            fill_n(back_inserter(ans), e.second, e.first);
+        }
+        
+        return ans;
+    }
+};
