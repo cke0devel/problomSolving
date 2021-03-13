fae2147da74e13cf11de8aee285d9b5efc09c40a
cke0devel <cke0devel@gmail.com>
2021-03-13 09:08:36 +0900

    [leetcode/wa] 73. Set Matrix Zeroes

--- leetcode/73_Set_Matrix_Zeroes.cc
+++ leetcode/73_Set_Matrix_Zeroes.cc
@@ -2,7 +2,7 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         for(int i=0; i<matrix.size(); i++) {
-            for(int j=0;j<matrix.size(); j++) {
+            for(int j=0;j<matrix[0].size(); j++) {
                 if(matrix[i][j] != 0) {
                     continue;
                 }
